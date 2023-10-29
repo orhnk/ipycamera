8714cacab04eed58ab61eb9b821a2f410d7d154a
utfeight <utfeightt@gmail.com>
2023-10-29 16:09:25 +0300

    style: remove unneeded comments

--- camera.py
+++ camera.py
@@ -43,33 +43,4 @@ while True:
         if cv2.waitKey(1) == 27:
             break
 
-cv2.destroyAllWindows()  #
-# import cv2
-# from urllib.request import urlopen
-# import numpy as np
-# import sys
-#
-# host = "192.168.1.4:8080"
-# if len(sys.argv) > 1:
-#     host = sys.argv[1]
-#
-# hoststr = 'http://' + host + '/video'
-# print('Streaming ' + hoststr)
-#
-# stream = urlopen(hoststr)
-#
-# # Initialize a buffer to store bytes
-# bytes_buffer = b''
-#
-# while True:
-#     bytes_buffer += stream.read(1024)
-#     a = bytes_buffer.find(b'\xff\xd8')
-#     b = bytes_buffer.find(b'\xff\xd9')
-#     if a != -1 and b != -1:
-#         jpg = bytes_buffer[a:b+2]
-#         bytes_buffer = bytes_buffer[b+2:]
-#         i = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
-#         cv2.imshow(hoststr, i)
-#         if cv2.waitKey(1) == 27:
-#             exit(0)
-#
+cv2.destroyAllWindows()
